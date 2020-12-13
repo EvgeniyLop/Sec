2790957d5b5396184d37ca2b256ad004faafbb3c
EvgeniyLop <70152659+EvgeniyLop@users.noreply.github.com>
2020-12-14 01:47:22 +0700

    Добавьте файлы проекта.

---
+++ Говнокод/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Говнокод //секундомер действия кода
+{
+    class Program
+    {
+        public static bool cod = true;
+        public static int sec = 0;
+        static void Main(string[] args)
+        {
+            Method();
+            Console.WriteLine("Программа начала работу");
+            for (int i = 0; i <=100000; i++)
+            {
+                Console.WriteLine(i);
+                
+            }
+            cod = false; 
+            Console.WriteLine("Программа работала {0}",sec);
+        }
+        async static void Method()
+        {
+            await Task.Run(() =>
+            {
+                while (cod == true)
+                {
+                    Thread.Sleep(1000);
+                    sec++; 
+                }
+                return sec; 
+            });
+        }
+
+    }
+}
